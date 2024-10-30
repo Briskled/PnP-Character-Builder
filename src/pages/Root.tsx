d4660d534abca47653bc1449e27da9394a480d76
Felix Lapp <felix.lapp@gmx.de>
2024-10-30 17:49:56 +0100

    Test download

---
+++ src/pages/Root.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button"
+import { useRef, useState } from "react"
+import generatePDF from "react-to-pdf"
+
+export const Root: React.FunctionComponent = ({props}) => {
+    const [count, setCount] = useState(0)
+    const targetRef = useRef()
+
+    return (
+      <>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <Button onClick={() => generatePDF(targetRef, {filename: 'page.pdf'})}>Download</Button>
+          <p>
+            This is a change that is new!
+          </p>
+        </div>
+        <div ref={targetRef}>
+            <p className="read-the-docs">
+            Click on the Vite and React logos to learn more. Lololol
+            </p>
+        </div>
+      </>
+    )
+}
